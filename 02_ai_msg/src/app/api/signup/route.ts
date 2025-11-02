408c807cb4aad4de042a787735b17745a0391f2a
Your Name <you@example.com>
2025-11-02 10:27:12 +0500

    half we make it

--- 02_ai_msg/src/app/api/signup/route.ts
+++ 02_ai_msg/src/app/api/signup/route.ts
@@ -9,13 +9,14 @@ export async function POST(request: Request) {
     try {
 
        const {username, email, password} = await request.json()
-
-       const existingUserVerify =  await UserModel.findOne({
+       
+       //check by username
+       const existingUserVerifyByusername =  await UserModel.findOne({
         username,
         isVerified:true
        })
 
-       if (existingUserVerify) {
+       if (existingUserVerifyByusername) {
         return Response.json(
             {
                 success: false,
@@ -27,7 +28,69 @@ export async function POST(request: Request) {
         )
        }
 
-       await UserModel.
+       // check new user by email
+       const existingUserByEmail =  await UserModel.findOne({email})
+
+       // verifyCode OPT
+       const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+       if (existingUserByEmail) {
+        // another if state
+        if (existingUserByEmail.isVerified) {
+            return Response.json({
+                success: false,
+                message: 'User already exits'
+            },{
+                status:400
+            })
+        }else{
+            const hashedPassword = await bcrypt.hash(password,10)
+            existingUserByEmail.password = hashedPassword;
+            existingUserByEmail.verifyCode = verifyCode;
+            existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+            await existingUserByEmail.save()
+        }
+       }else {
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const expiryDate = new Date()
+        expiryDate.setHours(expiryDate.getHours() + 1)
+
+        const newUser = new UserModel({
+            username,
+            email,
+            password: hashedPassword,
+            verifyCode,
+            verifyCodeExpiry: expiryDate,
+            isAcceptingMessage: true,
+            isVerified: false,
+            message: []
+        })
+
+        await newUser.save()
+
+        }
+
+        // send verification Email
+        const emailResponse = await sendVerificationEmail(
+            email,username,verifyCode
+        )
+
+        if (!emailResponse.success) {
+            return Response.json({
+                success:false,
+                message:emailResponse.message
+            },{
+                status:500
+            })
+        }
+
+        return Response.json({
+                success:true,
+                message: 'User reistered successfully. verify code'
+            },{
+                status:201
+            })
 
     } catch (error) {
 
