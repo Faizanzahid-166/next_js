efa2efb6b3436981cbc8cbedbc1b601bcae0af9c
Your Name <you@example.com>
2025-12-29 20:37:35 +0500

    fix

--- 02_chat_app/src/app/_not-found/page.jsx
+++
@@ -1,8 +0,0 @@
-// app/_not-found/page.js
-export default function NotFoundPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
-    </div>
-  );
-}
