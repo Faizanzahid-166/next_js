cd610c86e56e303cdc213473f2ad5d2e73a74755
Your Name <you@example.com>
2025-12-29 20:24:48 +0500

    fix

--- 02_chat_app/src/app/(user-pages)/admin/dashboard/page.jsx
+++ 02_chat_app/src/app/(user-pages)/admin/dashboard/page.jsx
@@ -1,26 +1,18 @@
 "use client";
 
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function AdminDashboardPage() {
-  const { user } = useSelector((state) => state.auth);
-
-  if (!user) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <p className="text-gray-700 text-lg">Loading user data...</p>
-      </div>
-    );
-  }
+ // const [user, setUser] = useState(null);
 
   return (
     <div className="bg-white p-8 rounded-xl shadow-md">
       <h1 className="text-2xl font-bold text-purple-700 mb-4">
-        Welcome, {user.name} 👋
+        Welcome, admin 👋
       </h1>
 
-      <p className="text-gray-700">Email: {user.email}</p>
-      <p className="text-gray-700">Role: {user.role}</p>
+      <p className="text-gray-700">Email no</p>
+      <p className="text-gray-700">Role: admin</p>
     </div>
   );
 }
